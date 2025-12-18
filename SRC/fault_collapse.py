4b40daa13d63edc5ecdebe71162b40b32e952639
anthony-broderick <atbrod20@gmail.com>
2025-12-18 00:03:32 -0500

    Changed fault collapsing to properly use equivalence collapsing and dominance collapsing

--- SRC/fault_collapse.py
+++ SRC/fault_collapse.py
@@ -1,74 +1,66 @@
 import globals
-
-fanout_PI_outputs = set()
+from collections import defaultdict
 
 def collapse_faults():
-    collapsed_set = set()
+    # create set of faults for each value in wire_values 
+    # (s-a-0 and s-a-1)
+    collapsed_faults = {
+        f"{wire}: s-a-{sa}"
+        for wire in globals.wire_values.keys()
+        for sa in (0, 1)
+    }
+
+    # build dict for efficient check for non PI fanouts later
+    wire_to_gates = defaultdict(list)
 
     for gate in globals.gates:
+        for wire in gate.inputs:
+            wire_to_gates[wire].append(gate)
+    
+    PI_fanout_gates = set()
+
+    for gate in globals.gates:
+        if gate.gate_type == "fanout" and gate.inputs[0] in globals.primary_inputs:
+            for out in gate.output:
+                for g in wire_to_gates.get(out, []):
+                    PI_fanout_gates.add(g)
+
+    for gate in PI_fanout_gates:
+        print(gate.name)
+
+    for gate in globals.gates:
+        check = False
         if gate.gate_type == "fanout":
-            if gate.inputs[0] in globals.primary_inputs:
-                for out in gate.output:
-                    fanout_PI_outputs.add(out)
-
-    for gate in globals.gates:
-        if gate.PI == False:
             continue
-        collapsed_set |= collapse_faults_at_gate(gate)  
-    
-    globals.fault_list = collapsed_set
-
-def collapse_faults_at_gate(Gate):
-    collapsed_faults = set() # use a set to avoid duplicates
-
-    # NOT GATE
-    # inputs are functionally equivalent to the output inverted
-    # remove all input s-a-faults and keep output s-a-faults
-    if Gate.gate_type == "not":
-        collapsed_faults.add(f"{Gate.output}: s-a-0")
-        collapsed_faults.add(f"{Gate.output}: s-a-1")
-        return collapsed_faults
-
-    # FAULT EQUIVALENCE
-    # input s-a-(c) equivalent to output s-a-(c XOR i)
-    # remove both input s-a-(c)s and keep output s-a-(c XOR i)
-    # keep one of the input s-a-(c)s and remove output s-a-(c XOR i)
-
-    # FAULT DOMINANCE
-    # output s-a-(c XNOR i) dominates input s-a-(c')s
-    # remove output s-a-(c XNOR i) and keep both input s-a-(c')s
-
-    # after removing output s-a-(c XNOR i) and both input s-a-(c)s, we have:
-    # input s-a-(c')s
-    # one input s-a-(c)
-
-    check = False
-    for inp in Gate.inputs:
-        if inp in globals.primary_inputs:
-            if Gate.gate_type == "fanout":
+        if gate.gate_type == "not":
+            wire_name = f"{gate.output[0]}: s-a-0"
+            collapsed_faults.discard(wire_name)
+            wire_name = f"{gate.output[0]}: s-a-1"
+            collapsed_faults.discard(wire_name)
+            continue
+        # FAULT EQUIVALENCE
+        # input s-a-(c) equivalent to output s-a-(c XOR i)
+        # remove all but one input s-a-(c) and remove output s-a-(c XOR i)
+        for inp in gate.inputs:
+            wire_name = f"{inp}: s-a-{gate.c}"
+            # remove all non PI fanout fault equivalence
+            if (
+                '_fan' in wire_name 
+                and gate not in PI_fanout_gates
+            ):
+                collapsed_faults.discard(wire_name)
                 continue
-        
-            # always add input s-a-(c')s
-            collapsed_faults.add(f"{inp}: s-a-{Gate.c ^ 1}")
-            # use check to add only one input s-a-(c)
-            if not check:
-                collapsed_faults.add(f"{inp}: s-a-{Gate.c}")
+            if check == False and wire_name in collapsed_faults:
                 check = True
-        if inp in fanout_PI_outputs:
-            if Gate.gate_type == "fanout":
                 continue
-            # remove fanout suffix
-            primary_inp = inp.split("_fan")[0] 
-        
-            # always add input s-a-(c')s
-            collapsed_faults.add(f"{inp}: s-a-{Gate.c ^ 1}")
-            collapsed_faults.add(f"{primary_inp}: s-a-{Gate.c ^ 1}")
-            # use check to add only one input s-a-(c)
-            if not check:
-                collapsed_faults.add(f"{inp}: s-a-{Gate.c}")
-                collapsed_faults.add(f"{primary_inp}: s-a-{Gate.c}")
-                check = True
+            collapsed_faults.discard(wire_name)
+        for out in gate.output:
+            wire_name = f"{out}: s-a-{gate.c ^ gate.inv}"
+            collapsed_faults.discard(wire_name)
+            # FAULT DOMINANCE
+            # output s-a-(c XNOR i) dominates input s-a-(c')s
+            # remove output s-a-(c XNOR i) and keep both input s-a-(c')s
+            wire_name = f"{out}: s-a-{~(gate.c ^ gate.inv) & 1}"
+            collapsed_faults.discard(wire_name)
 
-    
-    #collapsed_faults.add(f"{Gate.output}: s-a-{Gate.c ^ Gate.inv}")
-    return collapsed_faults
+    globals.fault_list = collapsed_faults
